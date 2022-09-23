b3bbf92457955d420e5aa9e8a8a5f60ee47466dc
Mark Otway <mark@otway.com>
2022-09-24 00:41:18 +0100

    Don't trim

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-dotnet publish QbtManager -r linux-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish QbtManager -r win7-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish QbtManager -r osx-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish QbtManager -r linux-x64 -c Release /p:PublishSingleFile=true 
+dotnet publish QbtManager -r win7-x64 -c Release /p:PublishSingleFile=true 
+dotnet publish QbtManager -r osx-x64 -c Release /p:PublishSingleFile=true 
